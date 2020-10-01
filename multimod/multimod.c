0d69371fad3c80c4df80bc162dd071ee3947655d
tracer-nju <tracer@nju.edu.cn>
2020-10-01 18:36:05 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: b28551ad61474873ba030835d47be3e1     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:36:06 up  3:27,  1 user,  load average: 0.02, 0.05, 0.00

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -18,7 +18,10 @@ uint64_t mod(uint64_t x, uint64_t y)
 uint64_t addmod(uint64_t a, uint64_t b, uint64_t m)
 {
     uint64_t t=mod(a+b,m);
-    return mod(mod(t, m) + mod(MAXN, m), m);
+    if(a+b<a || a+b<b)
+        return mod(mod(t, m) + mod(MAXN, m), m);
+    else
+        return mod(mod(a, m) + mod(b, m), m); 
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m)
