35b14b1f7a026c9f689956c2b0ada78f685c2fb1
tracer-nju <tracer@nju.edu.cn>
2020-11-02 21:41:34 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: c5b06aab950e4eee8ea7299251232890     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:41:34 up  3:18,  1 user,  load average: 0.06, 0.06, 0.03

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -46,15 +46,15 @@ void *asm_memcpy(void *dest, const void *src, size_t n)
     while (n--)
         *dp++ = *sp++;*/
     asm(
-        "movq   %0,%%rax;"
-        "testq   %2,%2;"
+        "mov   %0,%%rax;"
+        "test   %2,%2;"
         "je     .L2;"
         "movl    $0x0,%%ecx;"
         ".L3:"
         "movzbl (%1,%%rcx,1),%%r8d;"
         "mov    %%r8b,(%%rax,%%rcx,1);"
         "add    $0x1,%%rcx;"
-        "cmpq    %2,%%rcx;"
+        "cmp    %2,%%rcx;"
         "jne    .L3;"
         ".L2:"
         "retq;"
