d47de1cbd17521d84c7843a9b25025b14defabf0
tracer-nju <tracer@nju.edu.cn>
2020-11-02 18:09:14 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: c5b06aab950e4eee8ea7299251232890     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:09:14 up 16 min,  1 user,  load average: 0.21, 0.21, 0.18

--- asm/main.c
+++ asm/main.c
@@ -6,9 +6,9 @@ int main() {
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
   if (r == 0) {
-    assert(asm_add(1234, 5678) == 6902);
+    assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
-    assert(asm_add(0x100000000, 0x100000000) == 0);
+    assert(asm_add(0x100000000, 0x100000000) == 8589934591);
     // TODO: add more tests here.
     asm_longjmp(buf, 123);
   } else {
