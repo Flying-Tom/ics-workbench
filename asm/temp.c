959e50ccd9f4c71efe6c9288f0cb589a8fbf898b
tracer-nju <tracer@nju.edu.cn>
2020-11-16 04:20:23 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7c3ca2f59f28464c922a5ff4c29db9ad   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-42-generic       Architecture: x86-64  04:20:23 up 6 min,  1 user,  load average: 0.84, 0.59, 0.31

--- asm/temp.c
+++ asm/temp.c
@@ -23,12 +23,12 @@ int64_t std_add(int64_t a,int64_t b)
 {
     return a+b;
 }
-/*
+
 int main()
 {
     //assert(inc(1) == 2);
-    //printf("%d\n", asm_add(0x10002, 0x1000000));
+    printf("%d\n", asm_add(0x10002, 0x1000000));
     assert(asm_add(114, 115) == 229);
-    //int64_t fff = std_add(0x100000000, 0x100000000);
-    //fff++;
-}*/
+    int64_t fff = std_add(0x100000000, 0x100000000);
+    fff++;
+}
