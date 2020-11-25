1b9069e02fe4c83ba47abf913f75178a5d653980
tracer-nju <tracer@nju.edu.cn>
2020-11-25 21:49:17 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: d223e74c0b554a83a0eb3e3ef6496de1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  21:49:18 up  6:08,  1 user,  load average: 0.24, 0.16, 0.07

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -14,17 +14,28 @@ int *sieve(int n)
     int *p = primes, *prime = primes - 1, temp;
     is_prime[2] = true;
     is_prime[3] = true;
-    /*for (int i = 4; i <= n; i += 2)
+    for (int i = 4; i <= n; i += 2)
         is_prime[i] = false;
     for (int i = 3; i <= n; i += 2)
-        is_prime[i] = true;*/
+        is_prime[i] = true;
     memset(is_prime, false, sizeof(is_prime));
     *p++ = 2;
-    for (int i = 3; i <= n; i += 2)
+    *p++ = 3;
+    *p++ = 5;
+    for (int i = 6; i <= n; i += 6)
     {
+        i--;
         if (is_prime[i])
             *p++ = i;
-
+        for (int j = 1; i * prime[j] <= n; j++)
+        {
+            is_prime[i * prime[j]] = false;
+            if (!(i % prime[j]))
+                break;
+        }
+        i+=2;
+        if (is_prime[i])
+            *p++ = i;
         for (int j = 1; i * prime[j] <= n; j++)
         {
             is_prime[i * prime[j]] = false;
