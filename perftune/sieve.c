f910397f2e453808d020a8a7ba0bb88119f15b78
tracer-nju <tracer@nju.edu.cn>
2020-11-25 08:26:40 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 1844ef0166e14271a5f96834b9e10680   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  08:26:40 up  1:06,  1 user,  load average: 0.02, 0.11, 0.09

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -12,22 +12,22 @@ int *sieve(int n)
 {
     assert(n + 1 < N);
     memset(is_prime, 1, sizeof(is_prime));
-    int itemp=5,jtemp=0,cnt=1;
-    primes[0]=2;
-    primes[1]=3;
+    int itemp = 5, jtemp = 0, cnt = 1;
+    primes[0] = 2;
+    primes[1] = 3;
     for (int i = 0; itemp <= n; i++)
     {
-        if (!(flags[i>>5]&(1<<(i&31))))
-            primes[++cnt-1] = itemp;
+        if (!(flags[i >> 5] & (1 << (i & 31))))
+            primes[++cnt - 1] = itemp;
         for (int j = 2; j <= cnt && itemp * prime[j] <= n; j++)
         {
             jtemp = (itemp * primes[j] - 5) >> 1;
-            jtemp - = jtemp / 3;
-            flags[jtemp>>5] | = 1<<(jtemp&31);
+            jtemp -= jtemp / 3;
+            flags[jtemp >> 5] | = 1 << (jtemp & 31);
             if (!(itemp % primes[j]))
                 break;
         }
-        itemp+=2 + ((i&1) << 1);
+        itemp += 2 + ((i & 1) << 1);
     }
     return primes;
 }
