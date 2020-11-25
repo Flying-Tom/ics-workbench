7974126513bbb103897f86476995d7afc619ce00
tracer-nju <tracer@nju.edu.cn>
2020-11-25 08:37:24 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 1844ef0166e14271a5f96834b9e10680   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  08:37:24 up  1:16,  1 user,  load average: 0.05, 0.06, 0.08

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -5,24 +5,23 @@
 
 #define N 10000000
 
-static int flags[N / 96 + 1];
+static bool is_prime[N];
 static int primes[N];
 
 int *sieve(int n)
 {
     assert(n + 1 < N);
+    memset(is_prime,true,sizeof(is_prime));
     int itemp = 5, jtemp = 0, cnt = 1;
     primes[0] = 2;
     primes[1] = 3;
     for (int i = 0; itemp <= n; i++)
     {
-        if (!(flags[i >> 5] & (1 << (i & 31))))
+        if (is_prime[i])
             primes[++cnt ] = itemp;
         for (int j = 2; j <= cnt && itemp * primes[j] <= n; j++)
         {
-            jtemp = (itemp * primes[j] - 5) >> 1;
-            jtemp -= jtemp / 3;
-            flags[jtemp >> 5] |= 1 << (jtemp & 31);
+            is_prime[i * prime[j]] = false;
             if (!(itemp % primes[j]))
                 break;
         }
