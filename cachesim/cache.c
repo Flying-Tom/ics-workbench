bb3e37f982d0da637992adef3bf0e2edd1b5e418
tracer-nju <tracer@nju.edu.cn>
2021-01-12 16:52:32 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 6074a24a09ea4dd2be50a226d1575e87   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-36-generic       Architecture: x86-64  16:52:33 up  7:22,  0 users,  load average: 0.15, 0.16, 0.06

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -10,17 +10,37 @@ void cycle_increase(int n) { cycle_cnt += n; }
 
 // TODO: implement the following functions
 
-struct cacheline
-{
-    bool flag;
-    bool data[BLOCK_WIDTH];
-};
+#define ADDR_TAG(addr) ((addr >> (GROUP_WIDTH + BLOCK_WIDTH)) & mask_with_len(TAG_WIDTH))
+#define ADDR_IDX(addr) ((addr >> BLOCK_WIDTH) & mask_with_len(GROUP_WIDTH))
+#define ADDR_INBLOCK(addr) (addr & mask_with_len(BLOCK_WIDTH))
+uint32_t group_linenum, GROUP_WIDTH, TAG_WIDTH;
+uint32_t total_line;
 
-struct cacheline* Cache;
+typedef struct
+{
+    bool valid_bit;
+    uint32_t tag;
+    bool data[BLOCK_WIDTH];
+} cacheline;
+
+struct cacheline *Cache;
+
+uint32_t cache_load(uintptr_t addr)
+{
+    return 0;
+}
 
 uint32_t cache_read(uintptr_t addr)
 {
-    return 0;
+    uint32_t group_idx = ADDR_IDX(addr);
+    cacheline *group_base = Cache[group_linenum * group_idx];
+
+    for (int i = 0; i < group_linenum; i++)
+    {
+        if (group_base[i].tag == ADDR_TAG(addr) && group_base[i].valid_bit)
+            return *(uint32_t *)group_base[i].data;
+    }
+    return *(uint32_t *)group_base[cache_load(addr)].data;
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask)
@@ -29,6 +49,10 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask)
 
 void init_cache(int total_size_width, int associativity_width)
 {
+    total_line = exp2(total_size_width) / BLOCK_SIZE;
+    GROUP_WIDTH = associativity_width;
+    group_linenum = exp2(associativity_width);
+    Cache = malloc(sizeof(cacheline) * total_line);
 }
 
 void display_statistic(void)
