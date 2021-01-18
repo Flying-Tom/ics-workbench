6859bfd696cccae28b2e74dfc0875892af49340c
tracer-nju <tracer@nju.edu.cn>
2021-01-18 16:23:07 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 8d7e4d820a2c49bbb02b7895435d179d   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  16:23:07 up  6:52,  0 users,  load average: 0.07, 0.05, 0.01

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -32,11 +32,12 @@ uint32_t cache_replace(uintptr_t addr)
 {
     uint32_t group_idx = ADDR_GRPIDX(addr);
     cacheline *group_base = &Cache[group_size * group_idx];
-    uint32_t rand_idx_ingroup = rand() % group_size;
 
+    uint32_t rand_idx_ingroup = rand() % group_size;
     printf("rand_idx_ingroup:%d\n", rand_idx_ingroup);
 
     uint32_t rand_idx_block = group_idx + (group_base[rand_idx_ingroup].tag << GROUP_WIDTH);
+    printf("rand_idx_block:%d\n", rand_idx_block);
 
     if (group_base[rand_idx_ingroup].dirty_bit)
         mem_write(rand_idx_block, group_base[rand_idx_ingroup].data);
