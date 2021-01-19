daf64cc91aaa5299751c1b2b7cf253209c69b930
tracer-nju <tracer@nju.edu.cn>
2021-01-19 16:06:42 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: a967549f8ee2495d9ecce4ac4454f66a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  16:06:42 up 17:09,  1 user,  load average: 0.17, 0.06, 0.03

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -83,14 +83,14 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask)
     {
         if (group_base[i].tag == ADDR_TAG(addr) && group_base[i].valid_bit)
         {
-            uint32_t *addr_temp = (uint32_t *)group_base[i].data[ADDR_INBLOCK(addr)];
+            uint32_t *addr_temp = (uint32_t *)(group_base[i].data + ADDR_INBLOCK(addr));
             *addr_temp = (*addr_temp & ~wmask) | (data & wmask);
             group_base[i].dirty_bit = true;
             return;
         }
     }
     uint32_t idx_ingroup = cache_load(addr);
-    uint32_t *addr_temp = (uint32_t *)&group_base[idx_ingroup].data[ADDR_INBLOCK(addr)];
+    uint32_t *addr_temp = (uint32_t *)(group_base[idx_ingroup].data + ADDR_INBLOCK(addr));
     *addr_temp = (*addr_temp & ~wmask) | (data & wmask);
     group_base[idx_ingroup].dirty_bit = true;
 }
